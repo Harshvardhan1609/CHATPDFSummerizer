9a9e8fc2abd2864be60771fa6bf69b94956aac0c
HARSHVARDHAN PUROHIT <75427196+Harshvardhan1609@users.noreply.github.com>
2023-06-23 13:14:56 +0530

    Add files via upload

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://media.licdn.com/dms/image/C5603AQHOvfkGr5wTFg/profile-displayphoto-shrink_400_400/0/1663264600559?e=1692835200&v=beta&t=P9LCOzmdCHA8C0j9DZFpF2u1XFZRlkFkI5sJZaaDjCM">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
